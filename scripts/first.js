8179a530c2bd13e40991cd9179857afffb31129b
s0wie <sowieh@gmail.com>
2021-11-30 09:11:15 +0100

    Made circles and one element move

--- scripts/first.js
+++ scripts/first.js
@@ -1,14 +1,27 @@
 function showCoords(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var coords = 'X coords: ' + x + ', Y coords: ' + y;
+  let x = event.clientX;
+  let y = event.clientY;
+  let coords = 'X coords: ' + x + ', Y coords: ' + y;
   document.getElementById('demo').innerHTML = coords;
   let newDiv = document.createElement('div');
-  const newContent = document.createTextNode('Hi there and greetings!');
-  newDiv.appendChild(newContent);
+  // const newContent = document.createTextNode('Hi there and greetings!');
+  // newDiv.appendChild(newContent);
   const container = document.querySelector('.container');
   document.body.insertBefore(newDiv, container);
+  // document.body.appendChild(newDiv);
   newDiv.classList.add('item-1');
   newDiv.style.top = `${y - 50}px`;
   newDiv.style.left = `${x - 50}px`;
 }
+
+function dropElements() {
+  elements.forEach((element) => {
+    element.classList.add('move-ltr');
+  });
+  console.log(elements);
+}
+
+window.addEventListener('keydown', dropElements);
+
+let elements = document.querySelectorAll('.item-1');
+console.log(elements);
