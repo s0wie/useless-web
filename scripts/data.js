262a1c331553e037acc25716550adeb483b0833d
s0wie <sowieh@gmail.com>
2021-12-12 22:11:14 +0100

    Changed from circles to balloons

---
+++ scripts/data.js
@@ -0,0 +1,49 @@
+// Array for mix-blend
+
+// Array for shapes
+
+// Array for sizes
+
+// Array for background colours on scroll
+const backgroundColours = [
+  'var(--black)',
+  'var(--purple)',
+  'var(--skyblue)',
+  'var(--turquoise)',
+  'var(--pink)',
+  'var(--orange)',
+  'var(--yellow)',
+  'var(--green)',
+  'var(--superyellow)',
+  'var(--black)',
+  'var(--red)',
+];
+
+// Array for different classes to the items with same properties
+const addClass = [
+  'item-1',
+  'item-2',
+  'item-3',
+  'item-4',
+  'item-5',
+  'item-6',
+  'item-7',
+  'item-8',
+  'item-9',
+  'item-10',
+];
+
+// different balloons
+const balloonUrl = [
+  'images/balloon-yellow.png',
+  'images/balloon-yellow.png',
+  'images/balloon-pink.png',
+  'images/balloon-purple.png',
+  'images/balloon-red.png',
+  'images/balloon-red.png',
+  'images/balloon-blue.png',
+  'images/balloon-blue.png',
+  'images/balloon-green.png',
+  'images/balloon-green.png',
+  'images/balloon-purple.png',
+];
--- scripts/first.js
+++ scripts/first.js
@@ -1,25 +1,78 @@
-function showCoords(event) {
+// VARIABLES
+const container = document.querySelector('.container');
+
+// EVENT LISTENERS
+window.addEventListener('keydown', dropElements);
+container.addEventListener('click', createShapes);
+window.addEventListener('mousewheel', changeBackground);
+
+// FUNCTIONS
+function createShapes(event) {
   let x = event.clientX;
   let y = event.clientY;
-  let coords = 'X coords: ' + x + ', Y coords: ' + y;
-  document.getElementById('demo').innerHTML = coords;
-  let newDiv = document.createElement('div');
-  // const newContent = document.createTextNode('Hi there and greetings!');
-  // newDiv.appendChild(newContent);
-  const container = document.querySelector('.container');
+  let coords = `coordinates: (${x}, ${y})`;
+  console.log(coords);
+  document.querySelector('.coords').innerHTML = coords;
+
+  let randomInt = Math.round(Math.random() * 10);
+  let newDiv = document.createElement('img');
+  newDiv.src = `${balloonUrl[randomInt]}`;
+  newDiv.width = 100;
   document.body.insertBefore(newDiv, container);
-  // document.body.appendChild(newDiv);
-  newDiv.classList.add('item-1');
+  newDiv.classList.add('positioning');
+  newDiv.classList.add(addClass[randomInt]);
   newDiv.style.top = `${y - 50}px`;
   newDiv.style.left = `${x - 50}px`;
 }
 
+// Ge dem random class namn t.ex item-5. Kör if-satser. If item-1 -> add 'move up';
+// if item-5 add "move-up-delay-slow"
 function dropElements() {
-  const elements = document.querySelectorAll('.item-1');
-  elements.forEach((element) => {
-    element.classList.add('move-ltr');
+  const itemsOne = document.querySelectorAll('.item-1');
+  const itemsTwo = document.querySelectorAll('.item-2');
+  const itemsThree = document.querySelectorAll('.item-3');
+  const itemsFour = document.querySelectorAll('.item-4');
+  const itemsFive = document.querySelectorAll('.item-5');
+  const itemsSix = document.querySelectorAll('.item-6');
+  const itemsSeven = document.querySelectorAll('.item-7');
+  const itemsEight = document.querySelectorAll('.item-8');
+  const itemsNine = document.querySelectorAll('.item-9');
+  const itemsTen = document.querySelectorAll('.item-10');
+  itemsOne.forEach((oneItem) => {
+    oneItem.classList.add('move-up');
   });
-  console.log(elements);
+
+  itemsTwo.forEach((twoItem) => {
+    twoItem.classList.add('move-up-delay');
+  });
+
+  itemsThree.forEach((threeItem) => {
+    threeItem.classList.add('move-up-fast');
+  });
+  itemsFour.forEach((fourItem) => {
+    fourItem.classList.add('move-up');
+  });
+  itemsFive.forEach((fiveItem) => {
+    fiveItem.classList.add('move-up-delay-2');
+  });
+  itemsSix.forEach((sixItem) => {
+    sixItem.classList.add('move-up-fast');
+  });
+  itemsSeven.forEach((sevenItem) => {
+    sevenItem.classList.add('move-up');
+  });
+  itemsEight.forEach((eightItem) => {
+    eightItem.classList.add('move-up-slow');
+  });
+  itemsNine.forEach((nineItem) => {
+    nineItem.classList.add('move-up-fast');
+  });
+  itemsTen.forEach((tenItem) => {
+    tenItem.classList.add('move-up-delay-3');
+  });
 }
 
-window.addEventListener('keydown', dropElements);
+function changeBackground() {
+  let randomInt = Math.round(Math.random() * 10);
+  document.body.style.background = backgroundColours[randomInt];
+}
